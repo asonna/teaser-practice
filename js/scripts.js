6a992486df67c33ed87b5612a479554fc142da90
Annie Sonna and Rafael Perez <annie@>
2016-10-18 09:35:07 -0700

    add less and more button to toggle practice

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 $(function() {
-  $("span.clickable").click(function() {
-    $("img").fadeToggle();
+  $("span#clickable1").click(function() {
+    $("span.hidden").fadeToggle();
+      $("span#clickable1").hide();
+    $("span#clickable2").show();
+  });
+  $("span#clickable2").click(function() {
+    $("span.hidden").fadeToggle();
+    $("span#clickable2").hide();
+    $("span#clickable1").show();
   });
 });
